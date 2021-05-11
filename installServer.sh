b514d279fa8d43003cc11fcd0fc152deb6f13598
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-05-11 12:22:30 -0400

    Boost swap again.
    This should be enough swap to fit most images now. We'll want 4GB+ RAM as well.

--- installServer.sh
+++ installServer.sh
@@ -7,7 +7,7 @@ sudo apt-get upgrade
 
 #Add Swap - Google Cloud has none by default.
 #This isn't *needed* but it can help prevent issues.
-sudo fallocate -l 2G /swapfile
+sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
