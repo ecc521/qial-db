dbba5ff8d58c4a71d58a3609b6a576d39c5fae72
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-06-22 17:42:12 -0400

    Update docker-bind.sh

--- docker-bind.sh
+++ docker-bind.sh
@@ -1,7 +1,4 @@
 #Use bind mounts.
-
-#TODO: Also bind packaged code and server. Can't bind node_modules (sharp has binaries), but can bind everything else.
-
 docker run \
 	--mount source="$(pwd)/data",target=/qial-db/data,type=bind  \
 	--mount source="$(pwd)/cache",target=/qial-db/cache,type=bind \
