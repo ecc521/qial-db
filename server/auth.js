21a699fdb9882d60cae242af8b77cc3154f65082
tjw47 <tjw47@dhe.duke.edu>
2021-08-14 20:10:36 -0400

    Fix broken login from loadDataCSV changes

--- server/auth.js
+++ server/auth.js
@@ -1,13 +1,17 @@
 const crypto = require("crypto")
 
 const loadDataCSV = require("./loadDataCSV.js")
+const csvParse = require('csv-parse/lib/sync')
 
 async function getAuthorizedUsers() {
-	let csv = await loadDataCSV(false, "Sheet1", `1Kk96cTM0LPZ54IKIXTPPymsejLsyW-Dh-CotgAVo6MM`)
-	let json = csv.json
+	let csvText = await loadDataCSV(false, "Sheet1", `1Kk96cTM0LPZ54IKIXTPPymsejLsyW-Dh-CotgAVo6MM`)
+	let arr = csvParse(csvText, {
+		columns: true,
+		columns_duplicates_to_array: true //Duplicate columns aren't yet supported here, but we might add something later.
+	})
 
 	let users = Object.create(null)
-	json.forEach((obj) => {
+	arr.forEach((obj) => {
 		users[obj.Name.toLowerCase()] = obj
 	})
 
--- server/loadDataCSV.js
+++ server/loadDataCSV.js
@@ -2,7 +2,6 @@ const fs = require("fs")
 const path = require("path")
 
 const fetch = require("node-fetch")
-const csvParse = require('csv-parse/lib/sync')
 
 let cache;
 let lastCached;
@@ -11,29 +10,6 @@ module.exports = async function loadDataCSV(useCache = false, SHEET_NAME="Mice",
 	let url = `https://docs.google.com/spreadsheets/d/${FILE_ID}/gviz/tq?tqx=out:csv&sheet=${SHEET_NAME}`
 	let response = await fetch(url)
 	let text = (await response.text()).trim()
-
-	// if (!cache || !useCache) {
-	// 	results = csvParse(text, {
-	// 		columns: true,
-	// 		columns_duplicates_to_array: true
-	// 	})
-	//
-	// 	//TODO: Process the CSV line by line instead.
-	// 	results.forEach((item, index) => {
-	// 		item.csvSources = item.csvSources || {}
-	// 		let lineNumber = index + 1
-	// 		let line = text.split("\n")[lineNumber]
-	//
-	// 		item.csvSources[SHEET_NAME] = {
-	// 			lineNumber,
-	// 			//Google Sheets is 1 indexed - add 1.
-	// 			editUrl: `https://docs.google.com/spreadsheets/d/${FILE_ID}/#gid=0&range=${lineNumber + 1}:${lineNumber + 1}`
-	// 		}
-	// 	})
-	//
-	// 	cache = {csvText: text, json: results}
-	// 	lastCached = Date.now()
-	// }
 
 	return text
 }
