a9ab35cbcb67deae775ca3c1369ba85a2795a00b
tjw47 <tjw47@dhe.duke.edu>
2021-08-16 11:06:59 -0400

    Re-enable CSV Downloads
    The new download tool auto-generates a "mega-CSV" - basically, the JSON converted into a CSV file that contains everything.

--- src/download.js
+++ src/download.js
@@ -19,15 +19,15 @@ downloadMenuDiv.appendChild(document.createElement("br"))
 let downloadFullJSON = document.createElement("button")
 downloadMenuDiv.appendChild(downloadFullJSON)
 
-// let downloadFullCSV = document.createElement("button")
-// downloadMenuDiv.appendChild(downloadFullCSV)
+let downloadFullCSV = document.createElement("button")
+downloadMenuDiv.appendChild(downloadFullCSV)
 
 downloadMenuDiv.appendChild(document.createElement("br"))
 let downloadSelectedJSON = document.createElement("button")
 downloadMenuDiv.appendChild(downloadSelectedJSON)
 
-// let downloadSelectedCSV = document.createElement("button")
-// downloadMenuDiv.appendChild(downloadSelectedCSV)
+let downloadSelectedCSV = document.createElement("button")
+downloadMenuDiv.appendChild(downloadSelectedCSV)
 
 downloadMenuAdditionalInfo = document.createElement("p")
 downloadMenuAdditionalInfo.innerHTML += `
@@ -97,42 +97,118 @@ downloadSelectedJSON.addEventListener("click", function() {
 	downloadFile("selectedmetadata.json", getSelectionJSONMetadata())
 })
 
-// function getFullCSVMetadata() {
-// 	return window.csvSources["Mice"]
-// }
-//
-// downloadFullCSV.addEventListener("click", function() {
-// 	downloadFile("fullmetadata.csv", getFullCSVMetadata())
-// })
-//
-// function getSelectionCSVMetadata() {
-// 	let itemsSelected = parentHolder.filter((item) => {
-// 		if (!item.checkbox.checked) {return false}
-// 		return true
-// 	}).map((item) => {return item.item})
-//
-// 	console.log(itemsSelected)
-//
-// 	let csvLines = window.csvSources["Mice"].split("\n")
-// 	let output = csvLines[0]
-//
-// 	itemsSelected.forEach((item) => {
-// 		if (item?.csvSources?.["Mice"].lineNumber) {
-// 			output += "\n" + csvLines[item?.csvSources?.["Mice"].lineNumber]
-// 		}
-// 	})
-// 	return output
-// }
-//
-// downloadSelectedCSV.addEventListener("click", function() {
-// 	downloadFile("selectedmetadata.csv", getSelectionCSVMetadata())
-// })
+function getAllProperties(items) {
+	//Returns a list of all the column names - used to determine the header for the CSV.
+	//Note that if a field contains arrays, we need to return enough copies of the column name for EVERY item to fit.
+	let props = new Map()
+	items.forEach((item) => {
+		Object.keys(item).forEach((key) => {
+			let currentAmount = props.get(key)
+
+			let itemVal = item[key]
+
+			if (itemVal instanceof Array) {
+				if (typeof itemVal[0] === "string" || typeof itemVal[0] === "number") {
+					//Ignore any arrays of Objects, etc.
+					//Also ensures that 0 length arrays can't happen, though I suspect they are filtered out MUCH earlier anyways.
+					props.set(key, Math.max(itemVal.length, currentAmount || 0))
+				}
+			}
+			else if (currentAmount === undefined) {
+				if (typeof itemVal === "string" || typeof itemVal === "number") {
+					props.set(key, 1)
+				}
+			}
+		})
+	})
+
+	return Array.from(props) //Returns an associative array - [[propertyName, numberOfTimes], [propertyName, numberOfTimes]]
+}
+
+function createCSV(items) {
+	//Based off the rules at https://www.freeformatter.com/csv-escape.html
+	//It appears there are two different ways of escaping things, several cases where escapes aren't technically needed.
+	//That said, we should err on the side of escaping too often. It doesn't do much harm.
+	//We may not even need to escape given current data, but it should absolutely be built in.
+
+	//TODO: This code needs redoing for associative arrays.
+
+	let str = ""
+
+	function escapeForCSV(str = "") {
+		//Escape using double quotes.
+		if (typeof str === "number") {str = String(str)}
+		if (str.includes(",") || str.includes("\n") || str.includes('"')) {
+			str = `"${str.replace('"', '""')}"`
+		}
+		return str
+	}
+
+	let props = getAllProperties(items)
+	props.forEach((assocArr, propIndex) => {
+		let prop = assocArr[0]
+		for (let i=0;i<assocArr[1];i++) {
+			if (propIndex !== 0 || i !== 0) {str += ","}
+			str += escapeForCSV(prop)
+		}
+	})
+
+	items.forEach((item) => {
+		str += "\n"
+		props.forEach((assocArr, propIndex) => {
+			let prop = assocArr[0]
+			let amount = assocArr[1]
+
+			let val = item[prop]
+			if (propIndex !== 0) {str += ","}
+
+			if (amount === 1) {
+				str += escapeForCSV(val)
+			}
+			else {
+				if (!(val instanceof Array)) {
+					val = [val]
+				}
+
+				for (let i=0;i < amount;i++) {
+					if (i !== 0) {str += ","}
+					str += escapeForCSV(val[i])
+				}
+			}
+		})
+	})
+
+	return str
+}
+
+function getFullCSVMetadata() {
+ 	return createCSV(window.data)
+}
+
+downloadFullCSV.addEventListener("click", function() {
+	downloadFile("fullmetadata.csv", getFullCSVMetadata())
+})
+
+function getSelectionCSVMetadata() {
+	let itemsSelected = parentHolder.filter((item) => {
+		if (!item.checkbox.checked) {return false}
+		return true
+	}).map((item) => {return item.item})
+
+	console.log(itemsSelected)
+
+	return createCSV(itemsSelected)
+}
+
+downloadSelectedCSV.addEventListener("click", function() {
+	downloadFile("selectedmetadata.csv", getSelectionCSVMetadata())
+})
 
 function offerAdditionalFiles() {
 	downloadFullJSON.innerHTML = `Download JSON Metadata File (${numberPrettyBytesSI(getFullJSONMetadata().length)})`
 	downloadSelectedJSON.innerHTML = `Download JSON Metadata for Selection (${numberPrettyBytesSI(getSelectionJSONMetadata().length)})`
-	// downloadFullCSV.innerHTML = `Download CSV Metadata File (${numberPrettyBytesSI(getFullCSVMetadata().length)})`
-	// downloadSelectedCSV.innerHTML = `Download CSV Metadata for Selection (${numberPrettyBytesSI(getSelectionCSVMetadata().length)})`
+	downloadFullCSV.innerHTML = `Download CSV Metadata File (${numberPrettyBytesSI(getFullCSVMetadata().length)})`
+	downloadSelectedCSV.innerHTML = `Download CSV Metadata for Selection (${numberPrettyBytesSI(getSelectionCSVMetadata().length)})`
 }
 
 function getDownloadData() {
