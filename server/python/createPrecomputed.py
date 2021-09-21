c062ab391e9034135c410aba0d85ae792f696e98
tjw47 <tjw47@dhe.duke.edu>
2021-09-20 22:24:32 -0400

    Delete unused percentiles code

--- server/python/createPrecomputed.py
+++ server/python/createPrecomputed.py
@@ -4,7 +4,6 @@
 from cloudvolume import CloudVolume
 from downsampling import downscaleAveraging, majorityAveraging
 from thumbnails import generateThumbnailsArray
-from percentiles import getPercentiles
 
 import numpy as np
 import pandas as pd
