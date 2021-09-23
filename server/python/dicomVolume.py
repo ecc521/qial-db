8e75f74eed8732b06187f21159027c42d21f165a
tjw47 <tjw47@dhe.duke.edu>
2021-09-22 21:30:28 -0400

    Make optimizing assumptions to dicomVolume.py
    Half memory requirements by not holding slices in memory
    Assumes Instance Numbers increment by 1 and start at 1.

--- server/python/dicomVolume.py
+++ server/python/dicomVolume.py
@@ -1,7 +1,6 @@
 #Takes a ZIP file path, reads all DICOMs in the zip, and assembles them into a 3D array.
 #Returns 3D array and scan resolution.
 
-#TODO: Memory usage could be significantly optimized (halfed), and streaming would be ideal.
 #TODO: Handle Image Orientation tag
 from zipfile import ZipFile
 from pydicom import dcmread
@@ -11,47 +10,41 @@ import io
 
 def processDicomZip(zipFilePath):
     zip = ZipFile(zipFilePath)
-    filesInZip = zip.infolist()
 
-    #TODO: Ideally, we would write these iteratively, rather than buffer in memory and write all at once.
-    #It's hard to tell exactly how many DICOMs we are going to have though.
-    dicomSlices = {}
-
-    for file in filesInZip:
-        #TODO: Decompression bombs?
+    dicomsInZip = []
+    for file in zip.infolist():
         if file.filename.endswith(".dcm"):
-            bytes = zip.read(file)
-            bytesStream = io.BytesIO(bytes)
-            parsedDicom = dcmread(bytesStream)
-            #TODO: Is this rescaled automatically? I believe so, but needs to be checked.
-            dicomSlices[parsedDicom.InstanceNumber] = parsedDicom.pixel_array
-            #Resolutions are millimeters
-            PixelSpacing = parsedDicom.PixelSpacing
-            SliceThickness = parsedDicom.SliceThickness
-
-
+            dicomsInZip.append(file)
         else:
             print("Unable to determine file extension for " + file.filename)
 
 
-    #Ensure keys are ordered property.
-    keys = list(dicomSlices.keys())
-    for i in range(len(keys)):
-        keys[i] = int(keys[i])
-    keys.sort()
+    #Assume accquisition number starts at 1, and there are no gaps.
+    #This assumption allows us to avoid parsing all headers before beginning assembly.
+
+    hasProcessed = False
+
+    for file in dicomsInZip:
+        #TODO: Decompression bombs?
+        bytes = zip.read(file)
+        bytesStream = io.BytesIO(bytes)
+        parsedDicom = dcmread(bytesStream)
+        #TODO: Is this rescaled automatically based on slope and intercept?
+
+        pixelArr = parsedDicom.pixel_array
+
+        if (hasProcessed == False):
+            hasProcessed = True
+            #Setup Numpy array.
+            arr = np.zeros([*pixelArr.shape, len(dicomsInZip)], dtype=pixelArr.dtype)
+
+            #Set resolutions - values are in millimeters
+            #Assume 1000mm if not specified. This should be blatantly enough wrong.
+            PixelSpacing = parsedDicom.PixelSpacing or [1000, 1000]
+            SliceThickness = parsedDicom.SliceThickness or 1000
 
 
-    sliceStack = []
-    for key in keys:
-        sliceStack.append(dicomSlices[key])
+        arr[:, :, parsedDicom.InstanceNumber - 1] = pixelArr
 
-    #TODO: It looks like numpy is allocating a large array here, rather than using references.
-    #We should consider limiting zip reads and dicom processing to only the header, then allocating array,
-    #then running through the keys and actually loading the full image data. That would half memory usage.
-    arr = np.stack(sliceStack, axis=-1) #-1 means last axis - we want the stack to be z.
-
-    #Assume 1000mm if not specified. This should be blatantly enough wrong.
-    PixelSpacing = PixelSpacing or [1000, 1000]
-    SliceThickness = SliceThickness or 1000
 
     return {"arr": arr, "resolution": [*PixelSpacing, SliceThickness]}
