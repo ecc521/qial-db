6e4ff74f5625539a56309c89eaed96ed9c2b426a
tjw47 <tjw47@dhe.duke.edu>
2022-04-30 16:14:57 -0400

    Update assureRelativePathSafe to have protected directory for secret variables, etc.

--- assureRelativePathSafe.js
+++ assureRelativePathSafe.js
@@ -2,12 +2,60 @@ import path from "path";
 
 //Used to protect against path traversal attacks.
 function assureRelativePathSafe(relSrc) {
-    let hypoDir = "/a/b"
-    let hypoDirSame = "\\a\\b"		// windows path.join will return "\\a\\b\\" from joining of "/a/b" and "/"
-    let absSrc = path.join(hypoDir, relSrc)
-    if (! (absSrc.startsWith(hypoDir) || absSrc.startsWith(hypoDirSame))) {
+    console.log(relSrc)
+    while (path.isAbsolute(relSrc)) {
+        //Cut off leading characters until not absolute.
+        //This is necessary to ensure normalize works properly.
+        //If the path is absolute, we will only reduce down to / in this processing, yet outside of assureRelativePathSafe all the parts will be considered.
+        //We should only need to trim one leading slash, though it is possible more have been added (and hence we loop until reltative path obtained)
+        relSrc = relSrc.slice(1)
+    }
+
+    let normalizedPath = path.normalize(relSrc)
+
+    console.log(relSrc, normalizedPath)
+
+
+    let testPath = path.join("a", "b")
+
+    if (!path.join(testPath, normalizedPath).startsWith(testPath)) {
+        //If the path does backwards directories, it will be disallowed.
         throw "Path Traversal Forbidden"
     }
+
+    if (normalizedPath.startsWith("protected")) {
+        //Ensure protected directory is protected.
+        throw "403 Forbidden"
+    }
+
+    return true
 }
+
+
+
+
+//Tests for assureRelativePathSafe
+assureRelativePathSafe("/index.html")
+
+try {
+    assureRelativePathSafe("/../priorDir.html")
+    throw "assureRelativePathSafe failed traversal check"
+}
+catch (e) {
+    if (e != "Path Traversal Forbidden") {throw e}
+}
+
+
+try {
+    assureRelativePathSafe("/protected/hidden.html")
+    throw "assureRelativePathSafe failed protected check"
+}
+catch (e) {
+    if (e != "403 Forbidden") {throw e}
+}
+//End of tests for assureRelativePathSafe
+
+
+
 
 export default assureRelativePathSafe
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ import requestHandler from "./requestHandler.js"
 
 import admin from "firebase-admin"
 
-const serviceAccount = JSON.parse(fs.readFileSync("./qial_db_firebase_adminsdk.json", {encoding: "utf-8"}));
+const serviceAccount = JSON.parse(fs.readFileSync("./protected/qial_db_firebase_adminsdk.json", {encoding: "utf-8"}));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
