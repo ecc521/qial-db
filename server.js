90aab3d08d6644657b51685a4c1f443af7326529
tjw47 <tjw47@dhe.duke.edu>
2022-05-08 08:47:40 -0400

    Refractor out auth code

--- server.js
+++ server.js
@@ -18,25 +18,15 @@ import serveIndex from "serve-index";
 import bodyParser from "body-parser";
 
 import assureRelativePathSafe from "./assureRelativePathSafe.js"
+import createAuthChecker from "createAuthChecker"
+
 import requestHandler from "./requestHandler.js"
-
-import admin from "firebase-admin"
-
-const serviceAccount = JSON.parse(fs.readFileSync("./protected/qial_db_firebase_adminsdk.json", {encoding: "utf-8"}));
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-const auth = admin.auth();
-
-const users = db.collection("users")
 
 
 
 global.dataDir = path.join(path.dirname((new URL(import.meta.url)).pathname), "data")
-fs.mkdirSync(global.dataDir, {recursive: true})
+global.tmpDir = path.join(global.dataDir, "tmp")
+global.studiesDir = path.join(path.join(global.dataDir, "studies"))
 
 //Delete the tmp dir in dataDir, if it exists. (upload cache)
 fs.rmSync(path.join(global.dataDir, "tmp"), {force: true, recursive: true})
@@ -69,41 +59,6 @@ app.use(compression({
 }))
 
 app.use(express.urlencoded({ extended: false }));
-
-function createAuthChecker(requiredPermissions = {}) {
-    return function checkAuth(req, res, next) {
-        if (req.headers.authtoken) {
-            auth.verifyIdToken(req.headers.authtoken)
-            .then((user) => {
-                users.get(user.uid).then((querySnapshot) => {
-                    let userDoc = querySnapshot.docs[0]
-                    let data = userDoc.data()
-                    console.log(data)
-
-                    let permissions = data.permissions
-
-                    for (let prop in requiredPermissions) {
-                        if (permissions[prop] !== requiredPermissions[prop]) {
-                            return res.status(403).send(`Permission ${prop} not posessed or insuffecient. `)
-                        }
-                    }
-
-                    req.session = {
-                        user,
-                        data
-                    }
-                    next()
-                }, (e) => {
-                    res.status(500).send('Error Verifying Sign In: ' + e.message)
-                })
-            }).catch(() => {
-                res.status(403).send('Sign In Invalid')
-            });
-        } else {
-            res.status(403).send('Not Signed In')
-        }
-    }
-}
 
 app.all("*", (req, res, next) => {
     res.set("Strict-Transport-Security", "max-age=" + 60 * 60 * 24 * 365) //1 year HSTS.
