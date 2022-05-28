49384ba450b0331997faa1a1863a5d41d9f64498
tjw47 <tjw47@dhe.duke.edu>
2022-05-28 15:03:47 -0400

    Update spearmanCorrelation.js
    Fix issues with esmodules

--- src/graphs/spearmanCorrelation.js
+++ src/graphs/spearmanCorrelation.js
@@ -1,11 +1,11 @@
 //https://stackoverflow.com/a/36303302/10965456
-//spearmanCorrelation is copied verbatim.
+//spearmanCorrelation was modified to add let declarations on variables to run in esm modules.
 function spearmanCorrelation(multiList, p1, p2){
-    N=multiList[p1].length;
-    order=[];
-    sum=0;
+    let N=multiList[p1].length;
+    let order=[];
+    let sum=0;
 
-    for(i=0;i<N;i++){
+    for(let i=0;i<N;i++){
         order.push([multiList[p1][i], multiList[p2][i]]);
     }
 
@@ -13,7 +13,7 @@ function spearmanCorrelation(multiList, p1, p2){
         return a[0]-b[0]
     });
 
-    for(i=0;i<N;i++){
+    for(let i=0;i<N;i++){
         order[i].push(i+1);
     }
 
@@ -21,15 +21,15 @@ function spearmanCorrelation(multiList, p1, p2){
         return a[1]-b[1]
     });
 
-    for(i=0;i<N;i++){
+    for(let i=0;i<N;i++){
         order[i].push(i+1);
     }
-    for(i=0;i<N;i++){
+    for(let i=0;i<N;i++){
         sum+=Math.pow((order[i][2])-(order[i][3]), 2);
 
     }
 
-    r=1-(6*sum/(N*(N*N-1)));
+    let r=1-(6*sum/(N*(N*N-1)));
 
     return r;
 }
